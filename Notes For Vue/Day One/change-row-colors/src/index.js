094d4a0d793b0b776f07ce7d4a0a7474d2fb666a
plum <2412926291@qq.com>
2022-12-09 00:48:08 +0800

    修改部分内容

--- Notes For Vue/Day One/change-row-colors/src/index.js
+++ Notes For Vue/Day One/change-row-colors/src/index.js
@@ -3,6 +3,6 @@ import $ from  'jquery'
 //定义jquery入口函数
 $(function(){
     $('li:odd').css('background-color','red');
-    $('li:even').css('background-color','pink');
+    $('li:even').css('background-color','blue');
 
 })
--- Notes For Vue/Day One/change-row-colors/webpack.config.js
+++ Notes For Vue/Day One/change-row-colors/webpack.config.js
@@ -1,3 +1,15 @@
+
+const path =  require('path')
 module.exports ={
-    mode: 'development'   //mode 用来指定构建模式 可选值有development 和 production
+    mode: 'development' ,  //mode 用来指定构建模式 可选值有development 和 production
+
+    entry: path.join(__dirname,'./src/index.js'),   //entry 指定要处理哪个文件
+    output: {
+        path: path.join(__dirname,'dist'),
+        filename: 'mian.js'  //生成的文件名
+    },
+    devServer: {
+        static: "./src"                              // 修改默认打开目录
+      }
+     
 }
