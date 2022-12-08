6e3a80b02781236525b4364062260714129da779
plum <2412926291@qq.com>
2022-12-09 00:14:35 +0800

    webpack 打包

---
+++ Notes For Vue/Day One/change-row-colors/src/index.js
@@ -0,0 +1,8 @@
+import $ from  'jquery' 
+
+//定义jquery入口函数
+$(function(){
+    $('li:odd').css('background-color','red');
+    $('li:even').css('background-color','pink');
+
+})
---
+++ Notes For Vue/Day One/change-row-colors/webpack.config.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mode: 'development'   //mode 用来指定构建模式 可选值有development 和 production
+}
