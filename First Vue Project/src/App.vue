f652f7df98765ecb3b270fe396183a94103ffc4d
plum <2412926291@qq.com>
2022-12-08 22:55:30 +0800

    什么也没做

--- First Vue Project/src/App.vue
+++ First Vue Project/src/App.vue
@@ -2,7 +2,25 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
-
+<script>
+export default {
+  computed: {
+    username() {
+      // 我们很快就会看到 `params` 是什么
+      return this.$route.params.username
+    },
+  },
+  methods: {
+    goToDashboard() {
+      if (isAuthenticated) {
+        this.$router.push('/dashboard')
+      } else {
+        this.$router.push('/login')
+      }
+    },
+  },
+}
+</script>
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
